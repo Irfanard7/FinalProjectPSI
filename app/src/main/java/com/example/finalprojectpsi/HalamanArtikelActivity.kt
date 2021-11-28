e572c21febe7c73fef4c3ba389114eca931cdd35
Irfanard_ <ardiansyahirfan87@gmail.com>
2021-11-28 22:21:50 +0700

    init

---
+++ app/src/main/java/com/example/finalprojectpsi/HalamanArtikelActivity.kt
@@ -0,0 +1,11 @@
+package com.example.finalprojectpsi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HalamanArtikelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_artikel)
+    }
+}
---
+++ app/src/main/java/com/example/finalprojectpsi/HalamanVideoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.finalprojectpsi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HalamanVideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_video)
+    }
+}
