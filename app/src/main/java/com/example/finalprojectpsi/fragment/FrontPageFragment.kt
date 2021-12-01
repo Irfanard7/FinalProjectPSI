e2ae82814e844eadc6a24be4513cf18e24587cbb
falvracoadin <falvracoadin@gmail.com>
2021-12-01 10:34:19 +0700

    test

--- app/src/main/java/com/example/finalprojectpsi/fragment/FrontPageFragment.kt
+++ app/src/main/java/com/example/finalprojectpsi/fragment/FrontPageFragment.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import com.example.finalprojectpsi.R
 import com.example.finalprojectpsi.adapter.MySliderImageAdapter
 import com.smarteist.autoimageslider.SliderView
+import org.w3c.dom.Text
 
 class FrontPageFragment : Fragment() ,View.OnClickListener {
     lateinit var littleBox1 : TextView
@@ -32,6 +33,9 @@ class FrontPageFragment : Fragment() ,View.OnClickListener {
         imageList.add("https://images.ctfassets.net/hrltx12pl8hq/4plHDVeTkWuFMihxQnzBSb/aea2f06d675c3d710d095306e377382f/shutterstock_554314555_copy.jpg")
         imageList.add("https://media.istockphoto.com/photos/child-hands-formig-heart-shape-picture-id951945718?k=6&m=951945718&s=612x612&w=0&h=ih-N7RytxrTfhDyvyTQCA5q5xKoJToKSYgdsJ_mHrv0=")
         setImageInSlider(imageList, imageSlider)
+        view.findViewById<TextView>(R.id.Terkonfirmasi).text = "Terkonfirmasi : "+323424
+        view.findViewById<TextView>(R.id.Sembuh).text = "Sembuh : " + 123415
+        view.findViewById<TextView>(R.id.Meninggal).text = "Meninggal : " + 9425
         return view
     }
 
